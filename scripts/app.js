99da0fc1fc8b73cbd44996ad21eeac3e308e673a
ichoras <70770128+ichoras@users.noreply.github.com>
2020-10-25 22:58:03 -0700

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,22 @@
+// ONLINE: you MUST fill in the following values 
+//               from Firebase/Firestore for your project 
+// PROJECT_ID,
+// WEB_API_KEY,
+// PROJECT_NUMBER
+//
+// go to... 
+// console.Firebase.google.com, then 
+//    click on the Gear Icon -> Project Settings -> General
+(function(window) {
+    'use strict';
+    window.FireBaseConfig = {
+        // apiKey omitted for security purposes for push to github (repo is public);
+        // actual apiKey was inputted during testing and worked correctly
+        apiKey: "WEB_API_KEY",
+        authDomain: "coffeerun-2a320.firebaseapp.com",
+        databaseURL: "https://coffeerun-2a320.firebaseio.com",
+        projectId: "coffeerun-2a320",
+        storageBucket: "coffeerun-2a320.appspot.com",
+        messagingSenderId: "522137873601",
+    };
+})(window);
